5694dd90b944a712f6e5007ef92fbff5c48f77b7
Kashvi Singh <127826843+scitech-butterfly@users.noreply.github.com>
2025-04-19 15:24:22 +0530

    Initiate connection with database

--- StudentOperations.java
+++ StudentOperations.java
@@ -1,1 +1,19 @@
+// StudentOperations.java
+import java.sql.*;
+import java.util.*;
 
+public class StudentOperations {
+    private Connection conn;
+    private static final String URL="jdbc:mysql://localhost:3306/db_schema";
+    private static final String USER="root";
+    private static final String PASSWORD="PASSWORD";
+
+    public StudentOperations() {
+        try {
+            // establish connection with DBMS
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
